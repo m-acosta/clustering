e67dfaa87a330358a1d7b163946c2ca444944d4a
Michael Acosta <mdacosta@cpp.edu>
2023-05-07 12:20:27 -0700

    Minor cleanup

--- clustering.py
+++ clustering.py
@@ -24,10 +24,6 @@ df = pd.read_csv('training_data.csv', sep=',', header=None) #reading the data by
 X_training = np.array(df.values)[:,:64]
 
 #run kmeans testing different k values from 2 until 20 clusters
-     #Use:  kmeans = KMeans(n_clusters=k, random_state=0)
-     #      kmeans.fit(X_training)
-     #--> add your Python code
-
 silhouette = {}
 max_score = 0
 best_k = 0
@@ -35,7 +31,6 @@ best_k = 0
 for k in range(2, 21):
      kmeans = KMeans(n_clusters=k, random_state=0)
      kmeans.fit(X_training)
-
 
      #for each k, calculate the silhouette_coefficient by using: silhouette_score(X_training, kmeans.labels_)
      #find which k maximizes the silhouette_coefficient
@@ -46,7 +41,7 @@ for k in range(2, 21):
 
 #use the k value that maximizes the silhouette coefficient
 print("The best k value is: " + str(best_k))
-kmeans = KMeans(n_clusters=best_k, random_state=0, n_init=10)
+kmeans = KMeans(n_clusters=best_k, random_state=0)
 kmeans.fit(X_training)
 
 # plot the value of the silhouette_coefficient for each k value of kmeans so that we can see the best k
